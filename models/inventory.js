0c27637de71753cc8d83b433db435928cfa985cb
PaulM <pmeehan456@gmail.com>
2019-03-28 21:29:23 -0400

    database

---
+++ models/inventory.js
@@ -0,0 +1,28 @@
+module.exports = function (sequelize, DataTypes) {
+
+    let Inventory = sequelize.define('Inventory', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        storeID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        price: {
+            type: DataTypes.FLOAT(6, 2),
+            allowNull: true
+        }
+    });
+
+    return Inventory;
+};
