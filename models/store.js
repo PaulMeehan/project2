c6ec4468f73194250518561572b31f18ab522c83
unknown <burkeamy30@gmail.com>
2019-03-30 12:26:17 -0400

    added search results and owner item entry form. May need updating

---
+++ models/store.js
@@ -0,0 +1,34 @@
+module.exports = function (sequelize, DataTypes) {
+
+    let Store = sequelize.define('Store', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        storeName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hours: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+
+
+    return Store;
+};
+
