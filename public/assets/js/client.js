96378c043f6ebe03feb98822a3d5ff3407a19eb9
Emma Dedmond <emmadedmond@yahoo.com>
2019-03-31 15:43:17 -0400

    fixed indentation and minor calls to the api

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,63 +1,62 @@
 $(document).ready(function () {
-    
-    const newSearch = {
-		  itemName: itemName,
-          category: category,
-          description: description,
-          price: price
-        }
-    
-    const newItem = {
-        name: $("#itemName").val().trim(),
-        category: $("#itemCategory").val().trim(),
-        description: $("#itemDescription").val().trim(),
-        price: $("#price").val().trim()
-    };
-    
-    //this is supposed to retrieve the data from a search
-    $("button").click(function(event) {
-        $.ajax("/api/inventory/search", {
-            type: "GET",
-            data: newSearch
-          }).then(() => {
-            console.log("searching");
-            // Reload the page to get the updated list
-            location.reload();
-          });
-        });
+  const newSearch = {
+    itemName: itemName,
+    category: category,
+    description: description,
+    price: price
+  };
 
-    //this is supposed to post an item to a store
-    $(".newItem").click(function () {
-        $.ajax("/api/inventory/search" {
-            type: "POST",
-            data: newItem 
-          }).then(() => {
-            console.log("added item" + id);
-            // Reload the page to get the updated list
-            location.reload();
-          });
+  const newItem = {
+    name: $('#itemName').val().trim(),
+    category: $('#itemCategory').val().trim(),
+    description: $('#itemDescription').val().trim(),
+    price: $('#price').val().trim()
+  };
+
+    // this is supposed to retrieve the data from a search
+  $('button').click(function (event) {
+    $.ajax('/api/inventory/search', {
+      type: 'GET',
+      data: newSearch
+    }).then(() => {
+      console.log('searching');
+      // Reload the page to get the updated list
+      location.reload();
     });
-        
-    //this is the delete item section
-    $(".delete-item").on("click", function(event) {
-            const id = $(this).data("id");
+  });
 
-            $.ajax("/api/inventory/search" + id, {
-                type: "DELETE"
-              }).then(() => {
-                console.log("deleted item", id);
-                // Reload the page to get the updated list
-                location.reload();
-        });
+  // this is supposed to post an item to a store
+  $('.newItem').click(function () {
+    $.ajax('/api/inventory', {
+      type: 'POST',
+      data: newItem
+    }).then(() => {
+      console.log('added item' + id);
+      // Reload the page to get the updated list
+      location.reload();
     });
+  });
 
-    //this is the item update section
-    $.ajax("/api/inventory/search" + id, {
-        type: "PUT",
-        data: newItem
-      }).then(() => {
-        console.log("updated" + id);
-        // Reload the page to get the updated list
-        location.reload();
-      });
-})   
+  // this is the delete item section
+  $('.delete-item').on('click', function (event) {
+    const id = $(this).data('id');
+
+    $.ajax('/api/inventory/' + id, {
+      type: 'DELETE'
+    }).then(() => {
+      console.log('deleted item', id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  // this is the item update section
+  $.ajax('/api/inventory/' + id, {
+    type: 'PUT',
+    data: newItem
+  }).then(() => {
+    console.log('updated' + id);
+    // Reload the page to get the updated list
+    location.reload();
+  });
+});
